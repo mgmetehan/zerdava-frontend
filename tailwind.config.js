c3af2c422e7092795200f65f419e24c9bcd388d6
mgmetehan <mgmetehan@hotmail.com>
2023-03-16 13:35:43 +0300

    Add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-color': '#005152',
+        'second-color': '#003A37',
+      },
+    }
   },
   plugins: [],
 }
